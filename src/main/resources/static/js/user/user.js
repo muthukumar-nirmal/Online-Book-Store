7960e8078e54c6837c1db1c6a8e6982abc3c969d
Muthukumar M <muthu_m@MEDUSA.LOCAL>
2017-08-07 10:37:26 +0530

    added listUser Ajax call

--- src/main/resources/static/js/user/user.js
+++ src/main/resources/static/js/user/user.js
@@ -3,6 +3,10 @@
  */
 var USER_BASE_URL = 'http://localhost:8080/api/user/';
 
+$(document).ready(function() {
+	$('#signinForm').show();
+});
+
 function login() {
 	var email = $('#inputEmail').val();
 	var password = $('#inputPassword').val();
@@ -45,4 +49,42 @@ function login() {
 			}
 		});
 	}
+}
+
+function listUser(){
+	$('#signinForm').hide();
+	$('#userTableInfo').show();
+	$.ajax({
+		type : 'GET',
+		url : USER_BASE_URL + 'list',
+		dataType : 'json',
+		async : false,
+		success : function(data){
+			console.log(data);
+			if(data != null){
+				var userInfo = "";
+				for(var index = 1; index < data.length; index++){
+					userInfo += '<tr><td>' + data.firstName + " " + data.lastName + '</td><td>' + data.email + '</td><td>' + data.role + '</td><td><a href="#" class="btn btn-primary" onClick="viewUser(\''+ data.id +'\')" style="padding: 3px 7px;">View</a></td></tr>';
+				}
+				$('#userInfo').append(userInfo);
+				
+				 // Display data in the dataTable
+				//  "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+                var table = $('#userTableInfo').DataTable({
+                    "bDestroy": true,
+                    "bAutoWidth": false,
+                    "bFilter": true,
+                    "bSort": true,
+                    "pageLength": 5,
+                    "aaSorting": [[0]],
+                    "aoColumns": [
+                        { "sWidth": '25%', "bSortable": true },
+                        { "sWidth": '25%', "bSortable": true },
+                        { "sWidth": '25%', "bSortable": true },
+                        { "sWidth": '3%', "bSortable": false }
+                    ]
+                });
+			}
+		}
+	});
 }
